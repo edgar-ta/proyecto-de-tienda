24c22cc5d45fa01fae9eba5a389bff8110a27e47
Edgar Trejo Avila <edgar.avila.rro@gmail.com>
2022-06-12 13:15:42 -0500

    Borrando todo porque voy a empezar de cero de nuevo xd

--- main.js
+++
@@ -1,7 +0,0 @@
-// no supe cómo usar links en vez de botones en el css y mejor les añadí un
-// listener en el javascript xd
-
-document.querySelectorAll(".post-button__container").forEach(container => {
-    container.addEventListener("click", () => document.location = container.getAttribute("href"));
-});
-
