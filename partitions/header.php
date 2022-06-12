0d50d0185aa75016f9b8f3c44563872747add9e1
Edgar Trejo Avila <edgar.avila.rro@gmail.com>
2022-06-12 14:23:34 -0500

    Messing Around

--- partitions/header.php
+++ partitions/header.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Moctezuma</title>
+    <link rel="stylesheet" href="/main.css">
     <link rel="stylesheet" href="/css/navbar.css">
     <link rel="stylesheet" href="/css/header.css">
     <link rel="stylesheet" href="/css/footer.css">
@@ -13,13 +14,13 @@
     <video src="/res/moctezuma.mp4" controls></video>
     <nav class="navbar">
         <a class="navbar__tab" href="/">Moctezuma Xocoyotzin</a>
-        <div class="navbar__sup-tab-container">
-            <a class="navbar__tab" href="#articles" >Artículos</a>
+        <div class="navbar__tab">
+            <a class="" href="#articles" >Artículos</a>
             <div class="navbar__sub-tab-container">
-                <a class="navbar__tab" href="/info/bibliography.php">Bibliografía</a>
-                <a class="navbar__tab" href="/info/descendants.php">Descendencia</a>
-                <a class="navbar__tab" href="/info/reignate.php">Reinado</a>
-                <a class="navbar__tab" href="/info/trivia.php">Trivia</a>
+                <a class="navbar__tab--sub" href="/info/bibliography.php">Bibliografía</a>
+                <a class="navbar__tab--sub" href="/info/descendants.php">Descendencia</a>
+                <a class="navbar__tab--sub" href="/info/reignate.php">Reinado</a>
+                <a class="navbar__tab--sub" href="/info/trivia.php">Trivia</a>
             </div>
         </div>
         <a class="navbar__tab" href="/about-nahuatl.php">Sobre el Náhuatl</a>
